60d0030799aa6d548be31c7cde18f211357d4d3f
Neil Medina <ngmedina14@gmail.com>
2022-03-03 05:31:47 +0800

    updated mobile scrolldown function

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -3,3 +3,34 @@
 particlesJS.load('', './assets/json/particlesjs-config.json', function () {
     console.log('particles.json loaded..');
 });
+
+let touchstartY = 0
+let touchendY = 0
+
+const slider = document.getElementById('panel')
+const history = document.getElementById('history')
+
+function handleGesture() {
+    distance = touchstartY - touchendY
+if (Math. abs(distance) > 200 ){
+    if (touchendY < touchstartY) {
+        history.scrollIntoView();
+        console.log("down")
+      }
+      if (touchendY > touchstartY) {
+        history.scrollIntoView();
+        console.log("up")
+      }
+}
+  
+}
+slider.ontouchstart = (e) => {
+  touchstartY = e.changedTouches[0].screenY
+
+}
+
+slider.ontouchend =(e) => {
+  touchendY = e.changedTouches[0].screenY
+    handleGesture()
+}
+
